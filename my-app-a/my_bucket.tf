178df3d3abbc216d37ba3f69d86b58ba18105909
Ian Hong <ianyoungjunehong@gmail.com>
2025-12-02 11:37:56 +0000

    init

--- my-app-a/my_bucket.tf
+++ my-app-a/my_bucket.tf
@@ -5,3 +5,12 @@ module "my_app_a_bucket_b" {
   name       = "my-test-bucket-12345" # Must be globally unique
   location   = "us-central1"
 }
+
+
+module "my_app_a_bucket_second_b" {
+  source = "github.com/ianhong/tf-module-b.git?ref=v1.1.0"
+
+  project_id = "my-gcp-project-id"
+  name       = "my-test-bucket-12345" # Must be globally unique
+  location   = "us-central1"
+}

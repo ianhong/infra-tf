3914071eb62d90c798a0b0bdfc283d05e7121244
Ian Hong <ianyoungjunehong@gmail.com>
2025-12-02 11:25:34 +0000

    my-data

---
+++ my-app-a/my-data.tf
@@ -0,0 +1,7 @@
+module "my_app_a_bucket_b" {
+  source = "github.com/ianhong/tf-module-b.git?ref=v1.0.0"
+
+  project_id = "my-gcp-project-id"
+  name       = "my-test-bucket-12345" # Must be globally unique
+  location   = "us-central1"
+}
